b05f6da98a1466dc74644d1a9cfd46535cfa9db0
Moderli <99080776+Moderli@users.noreply.github.com>
2023-09-12 22:10:01 +0530

    Update project1.py

--- python/project1.py
+++ python/project1.py
@@ -1,1 +1,16 @@
+#first project 
+#this is a comment
+#this program defines the process of the calculation for finding the area of the triangle
+import math
+#input sides 
+sa = int(input("please enter the first side : "))
+sb = int(input("please enter the second side : "))
+sc = int(input("please enter the first side : "))
 
+#process the sides in to a formula
+
+s = (sa + sb + sc)/2
+m = math.sqrt(s)
+area = (s*(s-sa)*(s-sb)*(s-sc)) ** 0.5
+
+print(area , m)   #prints the area of the triangle
