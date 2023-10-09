a9a626794e9c1df2c3c77525fc07bb737c4a2e9a
Moderli <99080776+Moderli@users.noreply.github.com>
2023-10-09 10:39:26 +0530

    Create project10.py
    a python program illustrating all logical operations

---
+++ python/project10.py
@@ -0,0 +1,6 @@
+a = int(input("enter first number: "))
+b = int(input("the second number: "))
+c =  a < b and  b < 10
+print("{0} and {1} : {2}".format(a,b,c))
+d = a < b or b < 10 
+print("{0} or {1} : {2}".format(a,b,d))
